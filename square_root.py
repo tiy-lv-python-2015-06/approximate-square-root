d99c4a3f8ed4b9d8f9038c6815de0aa7b02c47cd
MARGARET MORRIS <margaretblue@MARGARETs-MacBook-Pro.local>
2015-06-29 13:15:43 -0700

    Added partial square_root() for first_guess

--- square_root.py
+++ square_root.py
@@ -1,5 +1,5 @@
 # script that asks the user for a positive number
-# and then compute the square root with a maximum error of 0.01.
+# then use Newton's Method to find square root with a maximum error of 0.01.
 # You then print out your answer to the user.
 
 #get_num() asks for positive number and validates input
@@ -12,7 +12,17 @@ def get_num():
             break
         except:
             print("\nPlease make sure it is a number and it is positive. ")
-            raw_input_num = input("\nPick a positive number ")
+            raw_input_num = input("\nPick a positive number: ")
         #print("End of try-except")
 
-        
+def square_root():
+    input_num = get_num()
+    # x = number you want root of
+    # y = guess
+    # guess = average = (y, x/y)
+    #first you need a guess. HOW? random? half it?
+    first_guess = input_num / 2.0
+    print("First Guess is {}".format(first_guess))
+
+
+square_root()
