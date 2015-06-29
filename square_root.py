2eb1e1e2a497d7280a233cc914dbf71749825001
MARGARET MORRIS <margaretblue@MARGARETs-MacBook-Pro.local>
2015-06-29 16:14:29 -0700

    Hard Mode success, added counter and negative check

--- square_root.py
+++ square_root.py
@@ -4,11 +4,14 @@
 
 #get_num() asks for positive number and validates input
 def choose_num():
-    raw_input_num = input("\nPick a positive number: ")
     while True:
+        raw_input_num = input("\nPick a positive number: ")
         try:
             input_num = float(raw_input_num)
-            return input_num
+            if input_num >= 0:
+                return input_num
+            else:
+                print("\nPlease pick a POSITIVE number")
         except:
             print("\nPlease make sure it is a number and it is positive. ")
             raw_input_num = input("\nPick a positive number: ")
@@ -17,21 +20,16 @@ def choose_num():
 def square_root():
     epsilon = 0.01
     picked_num = choose_num()
-    # x = number you want root of
-    # y = guess
-    # next guess = average of (y, x/y)
-    #first you need a guess. just pick the number 1
-    # first_guess = picked_num / 2.0
+    counter = 1
     guess = 1
     #test my guess
     test_my_guess = picked_num - (guess * guess)
     print("test_my_guess is {}".format(test_my_guess))
-    while test_my_guess > 0.01:
-    #while test_my_guess > 0.1:
-        #next guess is = (first_guess + (picked_num/first_guess)/2 )
+    while test_my_guess > epsilon:
         guess = ((guess) + (picked_num/guess)) / 2
         test_my_guess = (guess * guess) - picked_num
-        print("This Guess is {}, test_my_guess is {}".format(guess, test_my_guess))
+        print("Guess #{} is {}, epsilon is {}".format(counter, guess, test_my_guess))
+        counter += 1
     print("The square root of {} is {} ".format(picked_num, guess))
 
 square_root()
