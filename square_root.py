e89156b8a6f01835c39106a2d43ccb5465edfdd5
Aaron Pentz <AaronPentz@Aarons-MacBook-Pro.local>
2015-06-29 22:49:33 -0700

    Added input check for none intergers.

--- square_root.py
+++ square_root.py
@@ -1,15 +1,21 @@
-your_number = int(input("Please enter a Positive Number "))
-
+while True:
+    try:
+        your_number = int(input("Please enter a Positive Number ")) #This is where we get a Number
+    except ValueError:
+        print("Sorry that is not a valid entry")
+        continue
+    else:
+        break
 guess = 1
 epsilon = 0.001
 counter = 0
 
 while True:
-    how_far_away = abs((guess * guess) - your_number)
+    how_far_away = abs((guess * guess) - your_number) #This is to test the number
     if (how_far_away) <= epsilon:
         print(round(guess, 2))
         break
     else:
-        guess = (guess + your_number/guess) / 2
-        counter = counter + 1
+        guess = (guess + your_number/guess) / 2   #This is the new guess
+        counter = counter + 1  #Counter for number of passes
         print(counter, (round(guess, 2)))
