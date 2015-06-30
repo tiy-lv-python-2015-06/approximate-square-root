f4f01ccec714b4736b3ae131aeff57be9c8fa7ec
Aaron Pentz <AaronPentz@Aarons-MacBook-Pro.local>
2015-06-29 23:07:51 -0700

    Added some notes, cleand up naming.

--- square_root.py
+++ square_root.py
@@ -1,18 +1,24 @@
+
+#This is trying to see if you enter a number or not
 while True:
     try:
         your_number = int(input("Please enter a Positive Number ")) #This is where we get a Number
+        if your_number <= '0'
+        print("That is not a positive number. Please try again.")# Why cant I put an if statement here to check for a positive number?
+        break
     except ValueError:
         print("Sorry that is not a valid entry")
         continue
     else:
         break
+# Figure out how to take your_number and check if it is a negative
 guess = 1
 epsilon = 0.001
 counter = 0
 
 while True:
-    how_far_away = abs((guess * guess) - your_number) #This is to test the number
-    if (how_far_away) <= epsilon:
+    best_guess = abs((guess * guess) - your_number) #This is to test the number
+    if (best_guess) <= epsilon:
         print(round(guess, 2))
         break
     else:
